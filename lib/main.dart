7e3ce602ca5e8f6bc56a77ad57d50ff4df29a8bb
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2022-06-08 17:52:35 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Text('Lorem'),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              child: Text('Lorem'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
