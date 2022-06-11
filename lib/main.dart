e157b51e5788ee8360e11ee12748e1fd1d570912
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2022-06-11 20:06:39 +0200

    Modalne dodawanie transakcji

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,82 @@
-import 'package:expenses/widgets/user_transactions.dart';
+import 'package:expenses/widgets/new_transaction.dart';
+import 'package:expenses/widgets/transcation_list.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp() {
     initializeDateFormatting('pl_PL');
   }
 
-  String? titleInput;
-  String? amountInput;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Nowe szaty',
+      amount: 99.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Hot dog',
+      amount: 5.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+            child: NewTransaction(_addNewTransaction),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
         ),
         appBar: AppBar(
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: new Icon(Icons.add),
+            Builder(
+              builder: (context) => IconButton(
+                onPressed: () => _startAddNewTransaction(context),
+                icon: new Icon(Icons.add),
+              ),
             )
           ],
         ),
@@ -43,7 +92,7 @@ class MyApp extends StatelessWidget {
                   child: Text('Lorem'),
                 ),
               ),
-              UserTransactions(),
+              TransactionList(_transactions),
             ],
           ),
         ),
