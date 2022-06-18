66d47026a383c9ee89ad2ac7d637dc3b5a28c1f6
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2022-06-18 18:46:33 +0200

    Rozbudowa listy transakcji o key

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key? key,
     required this.tx,
@@ -16,48 +18,70 @@ class TransactionItem extends StatelessWidget {
   final MediaQueryData mediaQuery;
 
   @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  late Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.purpleAccent,
+      Colors.blueAccent,
+      Colors.amberAccent,
+      Colors.greenAccent,
+      Colors.deepPurpleAccent,
+      Colors.indigoAccent,
+    ];
+    _bgColor = availableColors[Random().nextInt(availableColors.length)];
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 10,
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       child: Dismissible(
-        key: Key(tx.id),
+        key: Key(widget.tx.id),
         onDismissed: (_) {
-          deleteTransactionHandler(tx.id);
+          widget.deleteTransactionHandler(widget.tx.id);
         },
         background: Container(
           color: Theme.of(context).errorColor,
         ),
         child: ListTile(
           leading: CircleAvatar(
+            backgroundColor: _bgColor,
             radius: 30,
             child: Padding(
               padding: const EdgeInsets.all(6),
               child: FittedBox(
-                child: Text('${tx.amount.toStringAsFixed(2)} zł'),
+                child: Text('${widget.tx.amount.toStringAsFixed(2)} zł'),
               ),
             ),
           ),
           title: Text(
-            tx.title,
+            widget.tx.title,
             style: Theme.of(context).textTheme.titleMedium,
           ),
           subtitle: Text(
-            DateFormat.yMMMMd('pl_PL').format(tx.date),
+            DateFormat.yMMMMd('pl_PL').format(widget.tx.date),
           ),
-          trailing: mediaQuery.size.width > 360
+          trailing: widget.mediaQuery.size.width > 360
               ? TextButton.icon(
-            onPressed: () => deleteTransactionHandler(tx.id),
-            icon: const Icon(Icons.delete),
-            label: const Text('Usuń'),
-          )
+                  onPressed: () => widget.deleteTransactionHandler(widget.tx.id),
+                  icon: const Icon(Icons.delete),
+                  label: const Text('Usuń'),
+                )
               : IconButton(
-            icon: const Icon(Icons.delete),
-            color: Theme.of(context).errorColor,
-            onPressed: () {
-              deleteTransactionHandler(tx.id);
-            },
-          ),
+                  icon: const Icon(Icons.delete),
+                  color: Theme.of(context).errorColor,
+                  onPressed: () {
+                    widget.deleteTransactionHandler(widget.tx.id);
+                  },
+                ),
         ),
       ),
     );
